1b5c6ccae14127c0075c407055c582a2aaef0045
mpatsis <mpatsis13@gmail.com>
2019-04-12 12:16:18 +0100

    TYPO in message.

--- bilm/training.py
+++ bilm/training.py
@@ -1069,7 +1069,7 @@ def test(options, ckpt_file, data, batch_size=256):
                 (batch_no, batch_perplexity, avg_perplexity, time.time() - t1))
 
     avg_loss = np.mean(batch_losses)
-    print("FINSIHED!  AVERAGE PERPLEXITY = %s" % np.exp(avg_loss))
+    print("FINISHED!  AVERAGE PERPLEXITY = %s" % np.exp(avg_loss))
 
     return np.exp(avg_loss)
 
